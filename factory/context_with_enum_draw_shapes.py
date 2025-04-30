6c8ce0855c1be00d99500050e2b4d2c2b8ab7f76
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2025-04-30 01:43:44 -0300

    feat: Adding ENUM

---
+++ factory/context_with_enum_draw_shapes.py
@@ -0,0 +1,94 @@
+import pygame
+import random
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+
+class ShapeType(Enum):
+    CIRCLE = auto()
+    RECTANGLE = auto()
+
+
+class Shape(ABC):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    @abstractmethod
+    def draw(self, surface):
+        pass
+    
+    
+class Circle(Shape):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.radius = random.randint(10, 50)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        
+        
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
+        
+        
+class Rectangle(Shape):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.width = random.randint(20, 100)
+        self.height = random.randint(20, 100)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        
+        
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
+        
+    
+    
+class ShapeFactory:
+    @staticmethod
+    def create_shape(shape_type, x, y):
+        if shape_type == ShapeType.CIRCLE:
+            return Circle(x, y)
+        elif shape_type == ShapeType.RECTANGLE:
+            return Rectangle(x, y)
+        else:
+            raise ValueError("Unknown shape type")
+        
+        
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    
+    pygame.display.set_caption("Shape Factory")
+    clock = pygame.time.Clock()
+    
+    shape_factory = ShapeFactory()
+    
+    shapes = []
+    
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                
+                shape_type = random.choice(["circle", "rectangle"])
+                shape = shape_factory.create_shape(shape_type, x, y)
+                shapes.append(shape)
+                
+        screen.fill((255, 255, 255))
+        
+        # buffer the shapes
+        for shape in shapes:
+            shape.draw(screen)
+            
+        pygame.display.flip()
+        clock.tick(60)
+        
+    pygame.quit()
+    
+    
+if __name__ == "__main__":
+    main()
